85c03ef3af61980380b1caf4c15cd11d6ffa4fdd
Revanza Firdaus <revanza.firdaus@gmail.com>
2026-04-19 20:47:29 +0700

    feat: add green background to MicrolockLogo widget

--- lib/widgets/microlock_logo.dart
+++ lib/widgets/microlock_logo.dart
@@ -9,12 +9,20 @@ class MicrolockLogo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset(
-      appLogoAsset,
+    return Container(
       height: height,
-      fit: BoxFit.contain,
-      filterQuality: FilterQuality.high,
-      semanticLabel: appName,
+      width: height * 1.83,
+      decoration: const BoxDecoration(
+        color: Color(0xFF10140F),
+        borderRadius: BorderRadius.all(Radius.circular(12)),
+      ),
+      child: Image.asset(
+        appLogoAsset,
+        height: height,
+        fit: BoxFit.contain,
+        filterQuality: FilterQuality.high,
+        semanticLabel: appName,
+      ),
     );
   }
 }
